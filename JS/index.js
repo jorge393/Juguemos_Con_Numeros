a4e9f9d5ced8ec038dbbd7c4c204bff9b64a5abb
LeonFBeymar <beymarleonf18@gmail.com>
2022-05-10 22:01:03 -0300

    color a los botones

---
+++ JS/index.js
@@ -0,0 +1,11 @@
+// var contenedores = document.getElementsByClassName("cont");
+
+// contenedores.array.forEach(element => {
+//     element.onfocus = function(){
+//         element.
+//     }
+// });
+
+// function cambiar (){
+    
+// }
