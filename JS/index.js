12d28000a03430dd73d60e988107bd59b8624d63
LeonFBeymar <beymarleonf18@gmail.com>
2022-05-16 12:02:17 -0300

    SIIIIUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU

--- JS/index.js
+++ JS/index.js
@@ -8,7 +8,7 @@ function cambiar2(){
     
     sound.play();
     setTimeout(() => {
-        location.href = "/HTML/Retos/RetoNumeros2.html";
+        location.href = "/RetoNumeros2.html";
     }, 1000);
 }
 
@@ -16,54 +16,54 @@ function cambiar3(){
     
     sound.play();
     setTimeout(() => {
-        location.href = "/HTML/Retos/RetoNumeros3.html";
+        location.href = "/RetoNumeros3.html";
     }, 1000);
 }
 function cambiar4(){
     
     sound.play();
     setTimeout(() => {
-        location.href = "/HTML/Retos/RetoNumeros4.html";
+        location.href = "/RetoNumeros4.html";
     }, 1000);
 }
 function cambiar5(){
     
     sound.play();
     setTimeout(() => {
-        location.href = "/HTML/Retos/RetoNumeros5.html";
+        location.href = "/RetoNumeros5.html";
     }, 1000);
 }
 function cambiar6(){
     
     sound.play();
     setTimeout(() => {
-        location.href = "/HTML/Retos/RetoNumeros6.html";
+        location.href = "/RetoNumeros6.html";
     }, 1000);
 }
 function cambiar7(){
     
     sound.play();
     setTimeout(() => {
-        location.href = "/HTML/Retos/RetoNumeros7.html";
+        location.href = "/RetoNumeros7.html";
     }, 1000);
 }
 function cambiar8(){
     
     sound.play();
     setTimeout(() => {
-        location.href = "/HTML/Retos/RetoNumeros8.html";
+        location.href = "/RetoNumeros8.html";
     }, 1000);
 }
 function cambiar9(){
     
     sound.play();
     setTimeout(() => {
-        location.href = "/HTML/Retos/RetoNumeros9.html";
+        location.href = "/RetoNumeros9.html";
     }, 1000);
 }
 function cambiarOtro(params) {
     sound.play();
     setTimeout(() => {
-        location.href = "/HTML/Menu2.html"
+        location.href = "/Menu2.html"
     }, 1000);
 }
