3cc7fe42a1f0a6b745f9f97b80842c550d51f0f2
LeonFBeymar <beymarleonf18@gmail.com>
2022-05-15 22:31:42 -0300

    LOS EXTRAÑO

--- JS/index.js
+++ JS/index.js
@@ -1,11 +1,13 @@
 var sound = new Audio();
     sound.src = "/SOUND/correcto.mp3";
+
+var soundError = new Audio();
+    soundError.src = "";
+
 function cambiarA(){
     
     sound.play();
     setTimeout(() => {
         location.href = "/HTML/Retos/RetoNumeros2.html";
     }, 3000);
-
-    
 }
