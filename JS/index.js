e33adf92a240de6085949a857afbb111b89c65d0
LeonFBeymar <beymarleonf18@gmail.com>
2022-05-16 12:18:46 -0300

    CABALLO

--- JS/index.js
+++ JS/index.js
@@ -1,14 +1,14 @@
 var sound = new Audio();
-    sound.src = "/SOUND/correcto.mp3";
+    sound.src = "./SOUND/correcto.mp3";
 
 var soundError = new Audio();
-    soundError.src = "/SOUND/incorrecto.mp3";
+    soundError.src = "./SOUND/incorrecto.mp3";
 
 function cambiar2(){
     
     sound.play();
     setTimeout(() => {
-        location.href = "/RetoNumeros2.html";
+        location.href = "./RetoNumeros2.html";
     }, 1000);
 }
 
@@ -16,54 +16,54 @@ function cambiar3(){
     
     sound.play();
     setTimeout(() => {
-        location.href = "/RetoNumeros3.html";
+        location.href = "./RetoNumeros3.html";
     }, 1000);
 }
 function cambiar4(){
     
     sound.play();
     setTimeout(() => {
-        location.href = "/RetoNumeros4.html";
+        location.href = "./RetoNumeros4.html";
     }, 1000);
 }
 function cambiar5(){
     
     sound.play();
     setTimeout(() => {
-        location.href = "/RetoNumeros5.html";
+        location.href = "./RetoNumeros5.html";
     }, 1000);
 }
 function cambiar6(){
     
     sound.play();
     setTimeout(() => {
-        location.href = "/RetoNumeros6.html";
+        location.href = "./RetoNumeros6.html";
     }, 1000);
 }
 function cambiar7(){
     
     sound.play();
     setTimeout(() => {
-        location.href = "/RetoNumeros7.html";
+        location.href = "./RetoNumeros7.html";
     }, 1000);
 }
 function cambiar8(){
     
     sound.play();
     setTimeout(() => {
-        location.href = "/RetoNumeros8.html";
+        location.href = "./RetoNumeros8.html";
     }, 1000);
 }
 function cambiar9(){
     
     sound.play();
     setTimeout(() => {
-        location.href = "/RetoNumeros9.html";
+        location.href = "./RetoNumeros9.html";
     }, 1000);
 }
 function cambiarOtro(params) {
     sound.play();
     setTimeout(() => {
-        location.href = "/Menu2.html"
+        location.href = "./Menu2.html"
     }, 1000);
 }
